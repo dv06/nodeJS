bb5d136e5c095bd7efc2ece62b5814b83076f6df
Devanshi Mishra <devanshi.mishra.06@gmail.com>
2022-03-05 16:18:10 +0530

    Adding custom command with args

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,13 +1,35 @@
 const chalk = require('chalk')
+const yargs = require('yargs')
 const getNotes  = require('./notes.js')
 
-const command = process.argv[2]
-
-if(command === 'add')
-{
-    console.log('Adding note')
-}
-else if(command === 'remove')
-{
-    console.log('Removing note')
-}
+// add, remove, read, list
+//create add command
+yargs.command({
+    command:'add',
+    describe:'Add a new note',
+    handler:function() {
+        console.log('Adding a new note!')
+    }
+})
+yargs.command({
+    command:'remove',
+    decribe:'Remove an existing note',
+    handler:function() {
+        console.log('Remove a note')
+    }
+})
+yargs.command({
+    command:'read',
+    decribe:'Read an existing note',
+    handler:function() {
+        console.log('Read a note')
+    }
+})
+yargs.command({
+    command:'list',
+    decribe:'List all nodes',
+    handler:function() {
+        console.log('List all nodes')
+    }
+})
+console.log(yargs.argv)
