5d171b6924966a55bd53246b64ce410478836cb3
Devanshi Mishra <devanshi.mishra.06@gmail.com>
2022-03-05 13:18:27 +0530

    Importing npm modules and working with them

--- app.js
+++
@@ -1,3 +0,0 @@
-const getNotes  = require('./notes.js')
-const notes = getNotes()
-console.log(notes)
---
+++ notes-app/app.js
@@ -0,0 +1,6 @@
+const validator = require('validator')
+const getNotes  = require('./notes.js')
+const notes = getNotes()
+console.log(notes)
+
+console.log(validator.isEmail('abc@xyz.com'))
