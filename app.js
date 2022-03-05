91985d91d22988a9af6c7634010d5af92230a028
Devanshi Mishra <devanshi.mishra.06@gmail.com>
2022-03-05 13:03:20 +0530

    Challenge 2

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-const add = require('./utils.js')
+const getNotes  = require('./notes.js')
+const notes = getNotes()
+console.log(notes)
 
-const sum  = add(4, -2)
-console.log(sum);
+// const add = require('./utils.js')
+
+// const sum  = add(4, -2)
+// console.log(sum);
