dc73db53da26f0abc73f3720f0e2de808532ff64
Devanshi Mishra <devanshi.mishra.06@gmail.com>
2022-03-05 16:53:13 +0530

    Reading data from file

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,7 +1,9 @@
 const fs = require('fs')
-const book = {
-    title:'Ego is the enemy',
-    author:'Ryan Holiday'
-}
-const bookJSON = JSON.stringify(book)
-fs.writeFileSync('1-json.json', bookJSON)
+// const book = {
+//     title:'Ego is the enemy',
+//     author:'Ryan Holiday'
+// }
+// const bookJSON = JSON.stringify(book)
+// fs.writeFileSync('1-json.json', bookJSON)
+const buffer = fs.readFileSync('1-json.json')
+console.log(buffer.toString())
