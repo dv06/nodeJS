1858a81cf5165ec4319b5831a8a99deec9f1f984
Devanshi Mishra <devanshi.mishra.06@gmail.com>
2022-03-05 15:47:11 +0530

    Working with command line args

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,13 @@
 const chalk = require('chalk')
 const getNotes  = require('./notes.js')
-const notes = getNotes()
-console.log(notes)
 
-console.log(chalk.green.bold.inverse('Success'))
+const command = process.argv[2]
+
+if(command === 'add')
+{
+    console.log('Adding note')
+}
+else if(command === 'remove')
+{
+    console.log('Removing note')
+}
