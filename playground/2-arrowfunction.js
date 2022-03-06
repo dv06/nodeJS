42e0c27db081db14487dcbbeb70db416b754c43f
Devanshi Mishra <devanshi.mishra.06@gmail.com>
2022-03-06 16:27:52 +0530

    Shorthand arrow function

--- playground/2-arrowfunction.js
+++ playground/2-arrowfunction.js
@@ -2,7 +2,10 @@
 //     return x* x
 // }
 
-const square = (x) => {
-    return x*x
-}
+// const square = (x) => {
+//     return x*x
+// }
+// console.log(square(3))
+
+const square = (x) => x*x
 console.log(square(3))
